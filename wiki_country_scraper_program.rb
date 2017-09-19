b5a6c4c8894f6705bb2554a6f9364f8baf531e66
timrooke1991 <timrooke1991@gmail.com>
2017-09-19 18:22:05 +0100

    Allows user inputs of translations that they would like

---
+++ wiki_country_scraper_program.rb
@@ -0,0 +1,79 @@
+require 'open-uri'
+require 'nokogiri'
+require 'csv'
+require 'mechanize'
+require 'logger'
+require 'io/console'
+
+mechanize = Mechanize.new
+
+puts "Please supply a list of languages: (e.g. German, Spanish)"
+languages = gets.chomp
+required_languages = languages.split(',').map { |country|
+  country.strip.capitalize
+}
+countries = []
+store_languages = {}
+required_languages.each_with_index do |country|
+  store_languages.merge!("#{country.downcase}": [])
+end
+
+### Need to create a new variable with the county names
+page = mechanize.get('file:///Users/TimRooke/Desktop/Coding/Ruby/activate_scraper/countries_A_C.html')
+index = 0
+
+rows_total = page.css('table tr').length
+
+while index < rows_total do
+  country = page.css('table tr')[index]
+
+  if country.css('td').length === 2
+    name = country.css('td a')[0].text
+
+    if name === ''
+      name = country.css('td').text
+      if name === ''
+        countries << name
+      else
+        countries << 'not found'
+      end
+    else
+      countries << name
+    end
+
+    required_languages.each do |c|
+      translations = country.css('td')[1].to_s
+      arr = translations.split('),').select {|e| e.include? c }
+      value = c.downcase
+      begin
+        if !arr.empty?
+          translated_name = arr.to_s.scan(/<([A-z0-9]*)\b[^>]*>(.*?)<\/\1>/)[-1][-1]
+          store_languages[value.to_sym] << translated_name
+        else
+          store_languages[value.to_sym] << 'not found'
+        end
+      rescue
+        store_languages[value.to_sym] << 'not found'
+      end
+    end
+  end
+  index += 1
+end
+
+puts "Creating .csv file"
+time = Time.now.strftime('%Y-%m-%d_%H-%M-%S')
+
+CSV.open("#{time}_countries.csv", "wb") do |file|
+	# Column Titles
+  row_headings = store_languages.keys.map { |key| "#{key.capitalize}" }
+  file << row_headings.unshift("Countries")
+
+	countries.length.times do |i|
+    # add row
+    row = []
+    store_languages.keys.each do |key|
+      row << store_languages[key.to_sym][i]
+    end
+		file << row.unshift(countries[i])
+	end
+end
